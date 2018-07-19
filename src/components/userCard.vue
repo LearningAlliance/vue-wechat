702bbad93eb81e173ab9f381689d3e4a6828a153
Kitten9533 <1173860556@qq.com>
2018-07-19 23:52:43 +0800

    complete userCard.vue

--- src/components/userCard.vue
+++ src/components/userCard.vue
@@ -1,27 +1,31 @@
 <template>
   <div class="section">
     <div class="card">
-      <div class="sign-in-box">已签到</div>
-      <div class="avatar">
-      	<!-- <image v-bind:src="userInfo.userHead" /> -->
-      	<img v-bind:src="'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532006746195&di=47e74eb4ed8a7b8e3befe47b19575533&imgtype=0&src=http%3A%2F%2Fcdnq.duitang.com%2Fuploads%2Fblog%2F201504%2F03%2F20150403225532_Jtxak.thumb.700_0.png'" />
-      </div>
-      <div class="user-name">
-      	<span>{{userInfo.userNick}}</span>
-      	<span>{{userInfo.userSex | showUserSex}}</span>
-      </div>
-      <div class="level">
-      	青铜会员
-      </div>
-      <div class="box">
-      	<div class="box-left">
-      		<p>保障金(元)</p>
-      		<p>2147.38</p>
-      	</div>
-      	<div class="box-right">
-      		<p>积分</p>
-      		<P>32310</P>
-      	</div>
+      <div class="card-box">
+        <div class="sign-in-box">已签到</div>
+        <div class="avatar">
+          <!-- <image v-bind:src="userInfo.userHead" /> -->
+          <img v-bind:src="'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532006746195&di=47e74eb4ed8a7b8e3befe47b19575533&imgtype=0&src=http%3A%2F%2Fcdnq.duitang.com%2Fuploads%2Fblog%2F201504%2F03%2F20150403225532_Jtxak.thumb.700_0.png'" />
+        </div>
+        <div class="user-name">
+          <span>{{userInfo.userNick}}</span>
+          <!-- <span>{{userInfo.userSex | showUserSex}}</span> -->
+        </div>
+        <div class="level">
+          <div class="level-box">
+            <span class="level-text">青铜会员</span>
+          </div>
+        </div>
+        <div class="box">
+          <div class="box-left">
+            <p class="box-title">保障金(元)</p>
+            <p class="box-content">2147.38</p>
+          </div>
+          <div class="box-right">
+            <p class="box-title">积分</p>
+            <P class="box-content">32310</P>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -62,26 +66,112 @@ export default {
   width: 100%;
   height: 536px;
   background: #fff;
-  .card{
-  	position: relative;
-  	margin: 30px auto;
-  	width: 690px;
-  	height: 476px;
-  	background-size: 100% 100%;
-  	background-repeat: no-repeat;
-  	// background-image: url('')
-  	border: 1px solid red;
-  	.avatar{
-  		width: 150px;
-  		height: 150px;
-  		margin: 40px auto 20px auto;
-  		img{
-  			display: block;
-  			width: 100%;
-  			height: 100%;	
-  			border-radius: 100%;
-  		}
-  	}
+  .card {
+    margin: 5px auto;
+    position: relative;
+    width: 730px;
+    height: 516px;
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    background-image: url('../assets/images/bg_my_topframe.png');
+    .card-box {
+      position: absolute;
+      top: 20px;
+      left: 20px;
+      width: 690px;
+      height: 476px;
+      .sign-in-box {
+        position: absolute;
+        top: 206px;
+        right: 0;
+        width: 130px;
+        height: 50px;
+        background: #F05720;
+        border-radius: 25px 0 0 25px;
+        color: #fff;
+        font-family: PingFangSC-Regular;
+        font-size: 24px;
+        line-height: 50px;
+        text-align: center;
+      }
+      .avatar {
+        width: 150px;
+        height: 150px;
+        margin: 40px auto 20px auto;
+        img {
+          display: block;
+          width: 100%;
+          height: 100%;
+          border-radius: 100%;
+        }
+      }
+      .user-name {
+        font-family: PingFangSC-Regular;
+        font-size: 32px;
+        color: #2E3141;
+        letter-spacing: 0;
+        text-align: center;
+        line-height: 41.6px;
+      }
+      .level {
+        width: 100%;
+        height: 40px;
+        margin-top: 10.4px;
+        font-family: PingFangSC-Regular;
+        text-align: center;
+        .level-box {
+          display: inline-block;
+          text-align: center;
+          height: 40px;
+          border: 2px solid #AF957E;
+          border-radius: 25px;
+          box-sizing: border-box;
+          .level-text {
+            display: inline-block;
+            font-family: PingFangSC-Regular;
+            font-size: 24px;
+            color: #AF957E;
+            line-height: 40px;
+            padding: 0 22px 0 22px;
+          }
+        }
+      }
+      .box {
+        width: 100%;
+        margin-top: 40px;
+        position: relative;
+        text-align: center;
+        .box-left {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 50%;
+        }
+        .box-right {
+          position: absolute;
+          top: 0;
+          right: 0;
+          width: 50%;
+        }
+        .box-title {
+          font-family: PingFangSC-Regular;
+          font-size: 28px;
+          color: #818B8F;
+          letter-spacing: 0;
+          text-align: center;
+          line-height: 36.4px;
+        }
+        .box-content {
+          margin-top: 10.6px;
+          font-family: PingFangSC-Semibold;
+          font-size: 36px;
+          color: #2E3141;
+          letter-spacing: 0;
+          text-align: center;
+          line-height: 46.8px;
+        }
+      }
+    }
   }
 }
 
--- src/pages/Mine.vue
+++ src/pages/Mine.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="page">
     <user-card></user-card>
+    <user-group :cell-lists="cellLists1"></user-group>
   </div>
 </template>
 <script type="text/javascript">
 import userCard from '@/components/userCard'
+import userGroup from '@/components/userGroup'
 import { mapGetters } from 'vuex'
 import api from '../fetch/api.js'
 
 export default {
   data() {
-    return {}
+    return {
+      cellLists1: [
+        { link: '', iconUrl: 'http://hzzly.net/xyy-vue/images/user-cert.png', text: '学生认证', desc: '', badg: '', more: true },
+        { link: 'user/info', iconUrl: 'http://hzzly.net/xyy-vue/images/user-detail.png', text: '我的详情', desc: '', badg: '', more: true },
+        { link: 'user/msg', iconUrl: 'http://hzzly.net/xyy-vue/images/user-message.png', text: '消息通知', desc: '', badg: '4', more: true },
+      ]
+    }
   },
   components: {
-    'user-card': userCard
+    'user-card': userCard,
+    'user-group': userGroup,
   },
   created() {
     api.getUserInfo({ userId: 33 }).then((res) => {
